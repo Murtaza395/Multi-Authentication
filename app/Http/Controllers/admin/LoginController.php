6eda955f611f48f477769fe5fed17b30c39c4431
Murtaza395 <mughalmurtaza999@gmail.com>
2024-08-16 00:48:42 +0500

    auth

---
+++ app/Http/Controllers/admin/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view("admin.login");
+    }
+    public function authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email'=>'required|email',
+            'password'=>'required',
+            'role'=>'required'
+        ]);
+        if($validator->passes()){
+            if(Auth::guard('admin')->attempt(['email'=> $request->email,'password'=> $request->password,'role'=>$request->role])){
+                if(Auth::guard('admin')->user()->role!="admin"){
+                    Auth::guard('admin')->logout();
+                return redirect()->route('admin.login')
+                ->with('error','You are are not authorized to access this page');
+        }
+        return redirect()->route('admin.dashboard');
+    }
+        else{
+            return redirect()->route('admin.login')
+            ->withErrors($validator)
+            ->withInput()->with('error','Either Email or Password is wrong!');
+        }
+    }
+    else{
+        return redirect()->route('admin.login')->withInput()->withErrors($validator);
+    }
+
+}
+public function logout(){
+    Auth::guard('admin')->logout();
+    return redirect()->route('admin.login');
+}
+}
